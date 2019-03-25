4bf64a090f4b4f1685ac2935755657a1b0b75f55
Patrick <pamarkham@hotmail.com>
2019-03-25 16:45:35 +0100

    prova1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+
+// app.module.ts
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { StudentModule } from './student/student.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent
+  }
+];
 
 @NgModule({
+
+  imports: [BrowserModule,RouterModule.forRoot(routes),StudentModule],
+  exports: [RouterModule],
+
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
